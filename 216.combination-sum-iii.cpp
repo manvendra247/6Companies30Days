d24472e414cec1caed3443220994d2091bf2c346
manvendra247 <76674381+manvendra247@users.noreply.github.com>
2023-01-08 16:05:18 +0000

    2nd question

---
+++ 216.combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>>ans;
+    void solve(int n , int i , int k, int sum,vector<int>&v){
+        if(sum==n&&k==0){
+            ans.push_back(v);
+            return ;
+        }
+        if(i>9||k<0)return;
+        for(int j=i;j<10;j++){
+            if(sum+j>n)return;
+            v.push_back(j);
+            solve(n,j+1,k-1,sum+j,v);
+            v.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int> v;
+        solve(n,1,k,0,v);
+        return ans;
+    }
+};
