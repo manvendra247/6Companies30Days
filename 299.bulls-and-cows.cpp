76cc3950d7e0ab598ff2cc3282f9887307927a81
manvendra247 <76674381+manvendra247@users.noreply.github.com>
2023-01-08 16:06:59 +0000

    3rd question

---
+++ 299.bulls-and-cows.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        int bulls = 0,cows = 0;
+        int digits1[10] = {0};
+        int digits2[10] = {0};
+        for(int i=0;i<secret.size();i++){
+            if(secret[i]==guess[i])bulls++;
+            else{
+                digits1[secret[i]-'0']++;
+                digits2[guess[i]-'0']++;
+            }
+        }
+        for(int i = 0;i<10;i++)cows+=min(digits1[i],digits2[i]);
+        return to_string(bulls)+"A"+to_string(cows)+"B";
+    }
+};
